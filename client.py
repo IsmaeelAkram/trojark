0add32d1b4011d7d2db6e59d91c1a34dbb154f73
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2021-06-24 14:32:08 -0400

    Fix dev

--- client.py
+++ client.py
@@ -6,9 +6,10 @@ import subprocess
 import sys
 from server_ip import server_ip
 
-ip = server_ip.server_ip if len(sys.argv) > 1 else socket.gethostname()
+verbose = True if len(sys.argv) > 1 else False
+ip = server_ip if len(sys.argv) > 1 else server_ip
+if verbose: print(ip)
 port = 9258
-verbose = False if len(sys.argv) > 1 else True
 
 print("Client has started, connecting to server")
 
